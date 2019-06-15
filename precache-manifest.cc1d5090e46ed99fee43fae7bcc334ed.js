9fbfc02292dbae417e367e6e36e5cea83306843d
Artur Michalak <artur93michalak@wp.pl>
2019-06-15 12:25:11 +0200

    Updates

---
+++ precache-manifest.cc1d5090e46ed99fee43fae7bcc334ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40411cc369a23cfc3e48b751c35c26b",
+    "url": "/Todo_app_react/./index.html"
+  },
+  {
+    "revision": "30e9499c9d20a76189da",
+    "url": "/Todo_app_react/./static/css/main.0d42779a.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/Todo_app_react/./static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "30e9499c9d20a76189da",
+    "url": "/Todo_app_react/./static/js/main.930a0090.chunk.js"
+  },
+  {
+    "revision": "7247ebe8ba65dac14af6",
+    "url": "/Todo_app_react/./static/js/runtime~main.779d857f.js"
+  }
+]);
